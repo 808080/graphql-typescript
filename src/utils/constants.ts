2ab275e347c98567a9e3b709dd35b7f0aedeb2e6
szaderaka <sergey.zaderaka@quadcode.com>
2022-05-09 16:05:42 +0300

    package.json script fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,5 +3,5 @@ import { config } from 'dotenv';
 config();
 
 export const APP_SECRET = process.env.SECRET_KEY;
-export const PORT = +process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3000;
 export const NODE_ENV = process.env.NODE_ENV;
