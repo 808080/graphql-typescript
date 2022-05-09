195628c87759e3ce7c1c3e199649dfc1b6f14737
szaderaka <sergey.zaderaka@quadcode.com>
2022-05-09 16:34:45 +0300

    default secret

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,6 @@ import { config } from 'dotenv';
 
 config();
 
-export const APP_SECRET = process.env.SECRET_KEY;
+export const APP_SECRET = process.env.SECRET_KEY || 'dev_secret';
 export const PORT = process.env.PORT || 3000;
 export const NODE_ENV = process.env.NODE_ENV;
